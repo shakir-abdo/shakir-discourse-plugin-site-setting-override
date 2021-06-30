158d4bc7a82ab4898ebfa12e43890ee177075a33
Shakir Abdo <shicolare1@gmail.com>
2021-06-30 19:51:28 +0200

    initial commit

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: shakir-discourse-plugin-site-setting-override
+# about: Example plugin to override a site setting
+# version: 1.0.0
+# authors: Joshua Rosenfeld Modified by Shakir abdo
+# url: https://github.com/shakir-abdo/shakir-discourse-plugin-site-setting-override
